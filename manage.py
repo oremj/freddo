69c7f2f22fb50814106d3ad19b457c1345025e0a
Jeremy Orem <jeremy.orem@gmail.com>
2011-03-31 13:28:34 -0700

    fixing up manage

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
 import imp
 import os
 import site
+import sys
 try:
     imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
@@ -10,13 +10,25 @@ except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
 
-import settings
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 path = lambda *a: os.path.join(ROOT, *a)
 
+prev_sys_path = list(sys.path)
+
 site.addsitedir(path('apps'))
 site.addsitedir(path('vendor/lib/python'))
+# Move the new items to the front of sys.path. (via virtualenv)
+
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+from django.core.management import execute_manager
+import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
