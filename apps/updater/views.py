166bc85744e2222d472a6e63ccfba662ea040644
Jeremy Orem <jeremy.orem@gmail.com>
2011-03-31 14:50:21 -0700

    exempt udpate_preview from csrf middleware

--- apps/updater/views.py
+++ apps/updater/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_exempt
 
 from . import tasks
 
 
 @never_cache
+@csrf_exempt
 def update_preview(request, app_name):
     tasks.update_app.delay(app_name)
     return HttpResponse('success')
