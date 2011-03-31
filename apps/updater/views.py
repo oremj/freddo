32c18d3fc9557184285f1a01f81774ddc596d9eb
Jeremy Orem <jeremy.orem@gmail.com>
2011-03-31 14:01:16 -0700

    don't cache update urls

--- apps/updater/views.py
+++ apps/updater/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.views.decorators.cache import never_cache
 
 from . import tasks
 
+
+@never_cache
 def update_preview(request, app_name):
     tasks.update_app.delay(app_name)
     return HttpResponse('success')
