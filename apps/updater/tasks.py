d5955e8c589433b72f14c9012333a25bd99c7a4c
Jeremiah Orem <jeremy.orem@gmail.com>
2011-05-27 13:15:57 -0700

    add option for "outfile"

--- apps/updater/tasks.py
+++ apps/updater/tasks.py
@@ -1,5 +1,5 @@
 import json
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
 
 from celery.task import task
 from django.conf import settings
@@ -7,7 +7,7 @@ import redisutils
 
 
 def run(script):
-    p = Popen(script, stdout=PIPE, stderr=PIPE, shell=True)
+    p = Popen(script, stdout=PIPE, stderr=STDOUT, shell=True)
     out, err = p.communicate()
     return p.returncode, out, err
 
@@ -17,13 +17,22 @@ def update_app(name, payload):
     redis = redisutils.connections['master']
 
     try:
-        script = settings.APP_UPDATERS[name]
+        app_config = settings.APP_UPDATERS[name]
     except KeyError:
         return
+
+    script = app_config['script']
+    outfile = app_config.get('outfile')
 
     l = update_app.get_logger()
     l.info("Running: %s" % script)
     redis.publish('update.%s' % name, json.dumps(payload))
+
     rv = run(script)
+
+    if outfile:
+        with open(outfile, 'w') as f:
+            f.write(rv[1])
+
     redis.publish('update.%s' % name, json.dumps(rv + (payload,)))
     l.info("Finished updating.")
